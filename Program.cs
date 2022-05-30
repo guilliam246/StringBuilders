28f971df381e34d04628ab88f8993d9f449c4523
Guilliam Oberholster <guilliamoberholster@gmail.com>
2022-05-30 15:58:39 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder("hello world");
+
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new String('-', 10));
+
+            Console.WriteLine(builder);
+
+            Console.WriteLine("First char " + builder[0]);
+
+        }
+    }
+}
